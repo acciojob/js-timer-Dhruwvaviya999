9f90f1256056b1bab52f6a532fd2e43767da3be5
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-26 02:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // write js code here if required
+
+let p = document.getElementById("timer");
+let now = new Date();
+
+p.textContent = `${now.getDay()}/${now.getMonth()}/${now.getYear()}, ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
